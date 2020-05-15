566107fb4774cb3b40e6103333611cdcf7974c14
Yakovlev471j <61596880+Yakovlev471j@users.noreply.github.com>
2020-05-15 15:11:50 +0300

    lab4

---
+++ лаб4/simple_match.py
@@ -0,0 +1,68 @@
+# Для каждого регулярного выражения, которое требуется написать,
+# указаны строки, соответствующие этому выражению (они отмечены знаком +),
+# а также строки, не соответствующие этому выражению (отмечены знаком -)
+
+# + a
+# + ab
+# - b
+# - ba
+REGEXP_1 = '^a[b]?'
+
+# + aab
+# + abb
+# + acb
+# - ab
+# - aabc
+REGEXP_2 = '^a[abc]b$'
+
+# + sofia.mp3
+# + sofia.mp4
+# - sofia.mp7
+# - sofia.mp34
+REGEXP_3 = r'^.*mp[3|4]$'
+
+# + taverna
+# + versus
+# + vera
+# + zveri
+# - zver
+REGEXP_4 = '^.*ver[^$].*'
+
+# - a
+# - aa
+# + aaa
+# - aaaa
+# - b
+# - bb
+# + bbb
+# - bbbb
+# - ccc
+REGEXP_5 = '^^[ab]{3}$'
+
+# - Ok
+# - OkOk
+# + OkOkOk
+# - OkOkOkOk
+# - ab
+# - abab
+# + ababab
+# - abababab
+REGEXP_6 = '^(Ok|ab){3}$'
+
+# - aaa
+# - aaa aaa
+# + aaa Aaa aaa
+# + aaa aaa Aaa
+# + Aaa aaa aaa
+# - A
+# - aaa A aaa
+REGEXP_7 = r'[aA]{3}\s[aA]{3}\s[aA]{3}'
+
+# + abc
+# + abc03
+# + a-b-c-3
+# + a.b.c.0
+# - Aabc
+# - abc1
+# - #abc
+REGEXP_8 = r'^a.*[^1]$'
